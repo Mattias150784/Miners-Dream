b9674831a0eeb8df3bb89ac610576afd37f6d6cc
MattiasAye <Mattiasayenew@gmail.com>
2024-08-21 12:15:37 -0700

    Recipe added for Miners TNT

--- src/main/java/net/mattias/minersdream/client/ClientEventHandler.java
+++ src/main/java/net/mattias/minersdream/client/ClientEventHandler.java
@@ -1,12 +1,15 @@
 package net.mattias.minersdream.client;
 
-import net.mattias.minersdream.MinersDream;
-import net.mattias.minersdream.client.MinersTNTRenderer;
 import net.mattias.minersdream.entities.ModEntities;
 import net.minecraftforge.client.event.EntityRenderersEvent;
 
 public class ClientEventHandler {
+
     public static void registerRenderers(EntityRenderersEvent.RegisterRenderers event) {
         event.registerEntityRenderer(ModEntities.MINERS_TNT_PRIMED.get(), MinersTNTRenderer::new);
+    }
+
+    public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(ModModelLayer.MINERS_TNT_LAYER, ModModelLayer::createMinersTNTLayer);
     }
 }
